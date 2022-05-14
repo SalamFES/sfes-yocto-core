a934ae587b3918460212a48c5ff30ca18dab55f2
Ahmad Moutraji <ahmadmoutraji@hotmail.com>
2022-05-14 09:55:07 +0200

    Added gitignore

--- scripts/setup-machine.sh
+++ scripts/setup-machine.sh
@@ -11,4 +11,4 @@ BRANCH=$(git symbolic-ref --short -q HEAD 2>/dev/null)
 ./scripts/repo init -u https://gitlab.com/ahmadBM/ebs-manifest -b $BRANCH -m ebscore/default.xml &&
 ./scripts/repo sync &&
 
-MACHINE=$1 source setup-environment ./$1-build
+MACHINE=$1 source scripts/setup-environment ./$1-build
