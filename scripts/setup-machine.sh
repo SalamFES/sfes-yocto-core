5851e9a78941d8edf000e32b2b51c63112f2afcf
Ahmad Moutraji <ahmadmoutraji@hotmail.com>
2022-07-19 22:45:45 +0200

    Added pn_001.xml

--- scripts/setup-machine.sh
+++ scripts/setup-machine.sh
@@ -9,7 +9,12 @@ fi
 BRANCH=$(git symbolic-ref --short -q HEAD 2>/dev/null)
 echo "BRANCH on $BRANCH"
 
-./scripts/repo init -u https://gitlab.com/ahmadBM/ebs-manifest -b $BRANCH -m ebscore/default.xml &&
+./scripts/repo init -u https://github.com/SalamFES/sfes-manifest.git -b $BRANCH -m pn_001.xml &&
 ./scripts/repo sync &&
 
+echo "Start setup-environment"
 MACHINE=$1 source scripts/setup-environment ./$2-build
+
+echo "Done."
+
+
